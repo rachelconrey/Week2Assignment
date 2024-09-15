bd699ad96705d221ebf253290404101630403a88
rachelconrey <163002900+rachelconrey@users.noreply.github.com>
2024-09-14 22:21:11 -0500

    Update helloworld.js
    updated response body

--- helloworld.js
+++ helloworld.js
@@ -7,7 +7,7 @@ http.createServer(function (request, response) {
    // Content Type: text/plain
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
-   // Send the response body as "Hello World"
+   // Send the response body as "Hello!"
    response.end('Good Morning World\n')
 }).listen(3000)
 
